2c622acfe2b692a57bd9312ab4d3c04826e8835a
Kirill Murzin <murz.kirill@yandex.ru>
2023-03-26 13:31:43 +0500

    html pages

---
+++ ui/static/scripts/index.js
@@ -0,0 +1,8 @@
+function show_text(){
+    document.getElementById('file-input-div').style.display ='none';
+    document.getElementById('text-input-div').style.display ='block';
+}
+function show_file_input(){
+    document.getElementById('text-input-div').style.display = 'none';
+    document.getElementById('file-input-div').style.display = 'block';
+}
